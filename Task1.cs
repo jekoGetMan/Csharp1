5493e4320f8d1e285ca5c19a9c77a5c3b2dd50cb
Вартанов Жора <sl1mvsshady@gmail.com>
2019-03-04 20:45:39 +0200

    new ideas

--- Task1.cs
+++ Task1.cs
@@ -1,35 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace HomeTask1
 {
-    class Program
+    internal class Program
+    {
+        public static void Main(string[] args)
         {
-        static void Main(string[] args)
-        {   
             Console.WriteLine("Please enter 5 numbers: ");
 
             var str = new string[5];
-            var num = new double[str.Length]; 
+            var num = new double[str.Length];
 
-            for (var i = 0; i < str.Length; i++)
+            for (var indx = 0; indx < str.Length; indx++)
             {
-                str[i] = Console.ReadLine();
-                num[i] = Convert.ToDouble(str[i]) * 10;    
+                str[indx] = Console.ReadLine();
+                try
+                {
+                    num[indx] = Convert.ToDouble(str[indx]) * 10;  // Без конвертации ругается
+                }
+                catch
+                {
+                    Console.WriteLine("We are working only with numbers");
+                    indx--;
+                }
             }
-
-            Console.WriteLine("Waaaaaaait...");
-
 
             foreach (var i in num)
             {
-
                 Console.Write(i + " ");
-
             }
+
             Console.ReadLine();
         }
     }
